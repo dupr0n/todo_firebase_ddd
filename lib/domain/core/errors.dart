3914c89087c7ad124b33b2bbbd1f691f39c22506
rkdupr0n <rkdupr0n@users.noreply.github.com>
2020-07-27 18:27:02 +0530

    Added authentication implementation. Added errors. Added firebase connection

---
+++ lib/domain/core/errors.dart
@@ -0,0 +1,13 @@
+import 'package:todo_firebase_ddd/domain/core/failures.dart';
+
+class UnexpectedValueError extends Error {
+  final ValueFailure valueFailure;
+
+  UnexpectedValueError(this.valueFailure);
+
+  @override
+  String toString() {
+    const String explanation = 'Encountered a ValueFailure at an unrecoverable point. Terminating';
+    return Error.safeToString('$explanation Failure: $valueFailure');
+  }
+}
--- lib/domain/core/value_object.dart
+++ lib/domain/core/value_object.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:todo_firebase_ddd/domain/core/errors.dart';
 
 import 'failures.dart';
 
@@ -9,6 +10,9 @@ abstract class ValueObject<T> {
   Either<ValueFailure<T>, T> get value;
 
   bool isValid() => value.isRight();
+
+  /// Throws [UnexpectedValueError] containing the [ValueError]
+  T getOrCrash() => value.fold((l) => throw UnexpectedValueError(l), id); //# id is same as (r) => r
 
   @override
   bool operator ==(Object o) {
