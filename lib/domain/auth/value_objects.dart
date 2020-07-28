6726fa37d42ab8990f44049ecc040c213d426b2a
rkdupr0n <rkdupr0n@users.noreply.github.com>
2020-07-28 17:23:55 +0530

    Added value failure for notes

--- lib/domain/auth/value_objects.dart
+++ lib/domain/auth/value_objects.dart
@@ -5,21 +5,23 @@ import '../core/value_object.dart';
 import '../core/value_validators.dart';
 
 class EmailAddress extends ValueObject<String> {
-  final Either<ValueFailure<String>, String> value;
-  const EmailAddress._(this.value);
-
   factory EmailAddress(String input) {
     assert(input != null);
     return EmailAddress._(validateEmailAddress(input));
   }
+
+  @override
+  final Either<ValueFailure<String>, String> value;
+  const EmailAddress._(this.value);
 }
 
 class Password extends ValueObject<String> {
-  final Either<ValueFailure<String>, String> value;
-  const Password._(this.value);
-
   factory Password(String input) {
     assert(input != null);
     return Password._(validateShortPassword(input));
   }
+
+  @override
+  final Either<ValueFailure<String>, String> value;
+  const Password._(this.value);
 }
--- lib/presentation/sign_in/widgets/sign_in_form.dart
+++ lib/presentation/sign_in/widgets/sign_in_form.dart
@@ -13,6 +13,7 @@ class SignInForm extends StatelessWidget {
         return Form(
           autovalidate: state.showErrorMessages,
           child: ListView(
+            padding: const EdgeInsets.all(8),
             children: <Widget>[
               const Text(
                 '📝',
@@ -81,7 +82,11 @@ class SignInForm extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              )
+              ),
+              if (state.isSubmitting) ...[
+                const SizedBox(height: 8),
+                const LinearProgressIndicator(),
+              ]
             ],
           ),
         );
