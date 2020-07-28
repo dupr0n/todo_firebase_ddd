c0391dcaaa655cc812c8173813e833f7e393ae86
rkdupr0n <rkdupr0n@users.noreply.github.com>
2020-07-28 20:53:38 +0530

    Added value objects for note. Added value transformer for note color. Added validation for note

--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -1,4 +1,5 @@
 import 'package:dartz/dartz.dart';
+import 'package:kt_dart/kt.dart';
 
 import 'failures.dart';
 
@@ -16,5 +17,43 @@ Either<ValueFailure<String>, String> validateShortPassword(String input) {
     return Right(input);
   } else {
     return Left(ValueFailure.shortPassword(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validateMaxStringLength(
+  String input,
+  int maxLength,
+) {
+  if (input.length <= maxLength) {
+    return right(input);
+  } else {
+    return left(ValueFailure.exceedingLength(failedValue: input, max: maxLength));
+  }
+}
+
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if (input.isNotEmpty) {
+    return right(input);
+  } else {
+    return left(ValueFailure.empty(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validateSingleLine(String input) {
+  if (!input.contains('\n')) {
+    return right(input);
+  } else {
+    return left(ValueFailure.multiline(failedValue: input));
+  }
+}
+
+Either<ValueFailure<KtList<T>>, KtList<T>> validateMaxListLength<T>(
+  KtList<T> input,
+  int maxLength,
+) {
+  if (input.size <= maxLength) {
+    return right(input);
+  } else {
+    return left(ValueFailure.listTooLong(failedValue: input, max: maxLength));
   }
 }
