2b15d18e5205314e30ad1baaf3c033d04766f0b0
rkdupr0n <rkdupr0n@protonmail.com>
2020-09-18 19:43:29 +0530

    Added AddTodo functionality

--- lib/presentation/notes/note_form/misc/todo_item_presentation_classes.dart
+++ lib/presentation/notes/note_form/misc/todo_item_presentation_classes.dart
@@ -1,10 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:kt_dart/collection.dart';
 
 import '../../../../domain/core/value_object.dart';
 import '../../../../domain/notes/todo_item.dart';
 import '../../../../domain/notes/value_objects.dart';
 
 part 'todo_item_presentation_classes.freezed.dart';
+
+class FormTodos extends ValueNotifier<KtList<TodoItemPrimitive>> {
+  FormTodos() : super(emptyList<TodoItemPrimitive>());
+}
 
 @freezed
 abstract class TodoItemPrimitive implements _$TodoItemPrimitive {
